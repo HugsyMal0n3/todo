c9c483b63effa991b6eda8a5537938b0f9cbb717
HugsyMal0n3 <electricnicks@gmail.com>
2025-09-15 13:33:11 +0700

    Refactor: dialog .html and .js
    Improve UX, layout, and efficency of JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css'
+
+const dialog = document.querySelector('dialog')
+
+const openDialogBtn = document.querySelector('#create-todo')
+openDialogBtn.addEventListener('mousedown', () => {
+  dialog.showModal()
+})
+
+const closeDialogBtn = document.querySelector('dialog button')
+closeDialogBtn.addEventListener('mousedown', () => {
+  dialog.close()
+})
